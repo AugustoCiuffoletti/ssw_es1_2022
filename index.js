c8ac24a349a63c779d963e1d4896abc8e613ebb6
AugustoCiuffoletti <augusto@di.unipi.it>
2022-02-16 15:03:12 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 // Import stylesheets
 import './style.css';
 const postiEl = document.getElementById('posti');
-const nomeEl = document.getElementById('nome');
 
-const posto="Alessio";
 var btn = document.createElement('button');
 btn.innerHTML = "P";
-btn.addEventListener('click', mostraNome);
 postiEl.appendChild(btn);
-
-function mostraNome() {
-  nomeEl.innerHTML = posto;
-}
