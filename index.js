a264088802bce2d4e024382eab1a19c5d3c74cc4
AugustoCiuffoletti <augusto@di.unipi.it>
2022-02-17 11:07:52 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const plateaEl = document.getElementById('platea');
 const palchiEl = document.getElementById('palchi');
-
 const nomeEl = document.getElementById('nome');
-
-const platea = [];
-
-for (var j = 0; j < 9; j++) {
-  platea.push(new Array(8));
-}
 
 class ordine {
   posti = [];
@@ -26,7 +19,9 @@ class ordine {
         let self = this;
         btn.addEventListener(
           'click', 
-          function() { nomeEl.innerHTML = self.posti[this.nfila][this.nposto] }
+          function() { 
+            nomeEl.innerHTML = self.posti[this.nfila][this.nposto]
+          }
         );
         this.element.appendChild(btn);
       }
@@ -37,21 +32,6 @@ class ordine {
 
 palchi = new ordine(4, 6, 'palchi');
 palchi.posti[2][2] = 'Luigi';
-
-//platea[2][1] = 'Alessio';
-platea[3][4] = 'Gianna';
-for (var j = 0; j < platea.length; j++) {
-  for (var i = 0; i < platea[j].length; i++) {
-    var btn = document.createElement('button');
-    btn.nfila = j;
-    btn.nposto = i;
-    btn.innerHTML = 'P' + (j + 1) + (i + 1);
-    btn.addEventListener('click', mostraNomePlatea);
-    plateaEl.appendChild(btn);
-  }
-  plateaEl.appendChild(document.createElement('br'));
-}
-
-function mostraNomePlatea() {
-  nomeEl.innerHTML = platea[this.nfila][this.nposto];
-}
+platea = new ordine(8, 8, 'platea');
+platea.posti[2][1] = 'Alessio';
+platea.posti[3][4] = 'Gianna';
