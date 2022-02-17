662797506ba504e392c8d886adee3b6acf98570c
AugustoCiuffoletti <augusto@di.unipi.it>
2022-02-17 10:57:12 +0100

    2 files modified

--- index.js
+++ index.js
@@ -4,24 +4,48 @@ const palchiEl = document.getElementById('palchi');
 const nomeEl = document.getElementById('nome');
 
 const platea = [];
-const palchi = [];
 
 for (var j = 0; j < 9; j++) {
   platea.push(new Array(8));
 }
 
-for (var j = 0; j < 6; j++) {
-  palchi.push(new Array(6));
+class ordine {
+  posti = [];
+  element;
+  constructor(nposti, nfile, elementName) {
+    this.element = document.getElementById(elementName);
+    for (var j = 0; j < nfile; j++) {
+      this.posti.push(new Array(nposti));
+    }
+    for (var j = 0; j < this.posti.length; j++) {
+      for (var i = 0; i < this.posti[j].length; i++) {
+        var btn = document.createElement('button');
+        btn.nfila = j;
+        btn.nposto = i;
+        btn.innerHTML = 'P' + (j + 1) + (i + 1);
+        let self = this;
+        btn.addEventListener(
+          'click', 
+          function() { nomeEl.innerHTML = self.posti[this.nfila][this.nposto] }
+        );
+        this.element.appendChild(btn);
+      }
+      this.element.appendChild(document.createElement('br'));
+    }
+  }
 }
 
-platea[2][1] = 'Alessio';
+palchi = new ordine(4, 6, 'palchi');
+palchi.posti[2][2] = 'Luigi';
+
+//platea[2][1] = 'Alessio';
 platea[3][4] = 'Gianna';
 for (var j = 0; j < platea.length; j++) {
   for (var i = 0; i < platea[j].length; i++) {
     var btn = document.createElement('button');
     btn.nfila = j;
     btn.nposto = i;
-    btn.innerHTML = 'P' +  (j + 1) + (i + 1);
+    btn.innerHTML = 'P' + (j + 1) + (i + 1);
     btn.addEventListener('click', mostraNomePlatea);
     plateaEl.appendChild(btn);
   }
@@ -30,21 +54,4 @@ for (var j = 0; j < platea.length; j++) {
 
 function mostraNomePlatea() {
   nomeEl.innerHTML = platea[this.nfila][this.nposto];
-}
-
-palchi[2][1] = 'Luigi';
-for (var j = 0; j < palchi.length; j++) {
-  for (var i = 0; i < palchi[j].length; i++) {
-    var btn = document.createElement('button');
-    btn.nfila = j;
-    btn.nposto = i;
-    btn.innerHTML = 'P' +  (j + 1) + (i + 1);
-    btn.addEventListener('click', mostraNomePalchi);
-    palchiEl.appendChild(btn);
-  }
-  palchiEl.appendChild(document.createElement('br'));
-}
-
-function mostraNomePalchi() {
-  nomeEl.innerHTML = palchi[this.nfila][this.nposto];
 }
