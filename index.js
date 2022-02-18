423eca9e6646f479c1ee77072e0fc5400fd68dbb
AugustoCiuffoletti <augusto@di.unipi.it>
2022-02-18 18:34:02 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const postiEl = document.getElementById('posti');
 const nomeEl = document.getElementById('nome');
 
 const posti = new Array(10);
-posti[2]="Alessio";
-posti[5]="Gianna";
 for (var i = 0; i < posti.length; i++) {
   var btn = document.createElement('button');
-  btn.indice = i;
   btn.innerHTML = "P"+ ( i + 1 );
+  btn.value="";
   btn.addEventListener('click', mostraNome);
   postiEl.appendChild(btn);
+  posti.push(btn);
 }
+posti[2].value="Alessio";
+posti[5].value="Gianna";
 
 function mostraNome() {
-  nomeEl.innerHTML = posti[this.indice];
+  nomeEl.innerHTML = this.value;
 }
