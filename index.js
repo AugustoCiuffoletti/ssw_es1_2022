5583a6a825fb4faa72d7066ead6f3e131832cdbb
AugustoCiuffoletti <augusto@di.unipi.it>
2022-02-18 19:02:35 +0100

    1 file modified

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const nomeEl = document.getElementById('nome');
 
 class ordine {
   posti = [];
-  element;
   constructor(nposti, nfile, elementName) {
     function mostraNome () { 
       if ( prenotaEl.value !== "" ) {
@@ -14,7 +13,7 @@ class ordine {
       else
         nomeEl.innerHTML = this.value 
     };
-    this.element = document.getElementById(elementName);
+    var element = document.getElementById(elementName);
     for (var j = 0; j < nfile; j++) {
       this.posti.push(new Array(nposti));
     }
@@ -23,15 +22,16 @@ class ordine {
         var btn = document.createElement('button');
         btn.innerHTML = 'P' + (j + 1) + (i + 1);
         btn.addEventListener('click', mostraNome);
-        this.element.appendChild(btn);
+        element.appendChild(btn);
+        this.posti[j][i]=btn
       }
-      this.element.appendChild(document.createElement('br'));
+      element.appendChild(document.createElement('br'));
     }
   }
 }
 
 palchi = new ordine(4, 6, 'palchi');
-palchi.posti[2][2] = 'Luigi';
+palchi.posti[2][2].value = 'Luigi';
 platea = new ordine(8, 8, 'platea');
-platea.posti[2][1] = 'Alessio';
-platea.posti[3][4] = 'Gianna';
+platea.posti[2][1].value = 'Alessio';
+platea.posti[3][4].value = 'Gianna';
