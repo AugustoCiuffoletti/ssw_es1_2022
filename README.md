dd5c4d4ca4b8ba7aae8c892090b4f1c05fcdce35
AugustoCiuffoletti <augusto@di.unipi.it>
2022-02-22 18:23:30 +0100

    2 files modified

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Esercizio di preparazione al progetto (Passo 7)
 
-Ora introduciamo un input box per inserire un nominativo da inserire nel nostro teatro. Inserito il nominativo si fa click su un posto e il nominativo viene registrato nell'array in corrispondenza al posto. Insieme, il colore del tasto diventa rosso per segnalare che il posto è occupato, e il contenuto dell'input box viene cancellato.
+Ora introduciamo un input box per inserire un nominativo da inserire nel nostro teatro. Inserito il nominativo si fa click su un posto e il nominativo viene registrato nel bottone in corrispondenza al posto. Insieme, il colore del tasto diventa rosso per segnalare che il posto è occupato, e il contenuto dell'input box viene cancellato.
+
+Attenzione, in questo modo gli array dei posti occupati non vengono aggiornati, ma questo non è un problema
--- index.js
+++ index.js
@@ -1,37 +1,56 @@
+class ordinePrenotazione {
+  prenotazione = [];
+  constructor(posti, elementName) {
+    
+    var element = document.getElementById(elementName);
+    this.prenotazione = posti.map((fila, i) => {
+      var p = fila.map((nome, j) => {
+        var btn = document.createElement('button');
+        element.appendChild(btn);
+        btn.value = nome;
+        btn.style.color = (nome !== "x") ? 'red' : 'green';
+        btn.innerHTML = 'P' + (j + 1) + (i + 1);
+        btn.addEventListener('click', this.mostraNome);
+        return btn;
+      });
+      element.appendChild(document.createElement('br'));
+      return p;
+    });
+  }
+  mostraNome () { 
+    if ( prenotaEl.value !== "" ) {
+      this.value = prenotaEl.value;
+      this.style.color = "red";
+      prenotaEl.value="";
+    }
+    else
+      nomeEl.innerHTML = this.value 
+  };
+}
+
 const prenotaEl = document.getElementById('prenota');
 const nomeEl = document.getElementById('nome');
 
-class ordine {
-  posti = [];
-  constructor(nposti, nfile, elementName) {
-    function mostraNome () { 
-      if ( prenotaEl.value !== "" ) {
-        this.value = prenotaEl.value;
-        this.style.color = "red";
-        prenotaEl.value="";
-      }
-      else
-        nomeEl.innerHTML = this.value 
-    };
-    var element = document.getElementById(elementName);
-    for (var j = 0; j < nfile; j++) {
-      this.posti.push(new Array(nposti));
-    }
-    for (var j = 0; j < this.posti.length; j++) {
-      for (var i = 0; i < this.posti[j].length; i++) {
-        var btn = document.createElement('button');
-        btn.innerHTML = 'P' + (j + 1) + (i + 1);
-        btn.addEventListener('click', mostraNome);
-        element.appendChild(btn);
-        this.posti[j][i]=btn
-      }
-      element.appendChild(document.createElement('br'));
-    }
-  }
+const teatro = {
+  platea: [],
+  palchi: []
+};
+
+const nfilePlatea = 7;
+const npostiPlatea = 10;
+for (var i = 0; i < nfilePlatea; i++) {
+  teatro.platea.push(new Array(npostiPlatea).fill('x'));
 }
 
-palchi = new ordine(4, 6, 'palchi');
-palchi.posti[2][2].value = 'Luigi';
-platea = new ordine(8, 8, 'platea');
-platea.posti[2][1].value = 'Alessio';
-platea.posti[3][4].value = 'Gianna';
+const nfilePalchi = 4;
+const npostiPalchi = 6;
+for (var i = 0; i < nfilePalchi; i++) {
+  teatro.palchi.push(new Array(npostiPalchi).fill('x'));
+}
+
+teatro.platea[2][1] = 'Alessio';
+teatro.platea[3][4] = 'Gianna';
+teatro.palchi[3][1] = 'Luigi';
+
+var plateaPrenotazione = new ordinePrenotazione(teatro.platea, 'platea');
+var palchiPrenotazione = new ordinePrenotazione(teatro.palchi, 'palchi');
