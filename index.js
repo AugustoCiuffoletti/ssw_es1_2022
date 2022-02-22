1ea30692b93afb62e2dd29d49a3673e8e1737dcb
AugustoCiuffoletti <augusto@di.unipi.it>
2022-02-22 13:06:58 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
-const nomeEl = document.getElementById('nome');
-
-for (var j = 0; j < nfilePlatea; j++) {
-  teatro.platea.push(new Array(npostiPlatea).fill("x"));
-}
-platea[2][1] = 'Alessio';
-platea[3][4] = 'Gianna';
-
 class ordine {
   prenotazione = [];
   constructor(posti, elementName) {
-    function mostraNome () { 
+    function mostraNome() {
       nomeEl.innerHTML = this.value;
-    };
+    }
     var element = document.getElementById(elementName);
 
-    prenotazione = posti.map( (fila,i) => {
-      p=fila.map( (nome,j) => {
+    prenotazione = posti.map((fila, i) => {
+      p = fila.map((nome, j) => {
         btn = document.createElement('button');
         plateaEl.appendChild(btn);
-        btn.value=nome;
-        btn.innerHTML = 'P' +  (j + 1) + (i + 1);
+        btn.value = nome;
+        btn.innerHTML = 'P' + (j + 1) + (i + 1);
         btn.addEventListener('click', mostraNome);
         return btn;
-      })
+      });
       plateaEl.appendChild(document.createElement('br'));
       return p;
-    })
-    
+    });
+
     function mostraNome() {
       nomeEl.innerHTML = this.value;
     }
   }
 }
 
-const teatro = { 
+const nomeEl = document.getElementById('nome');
+
+const nfilePlatea = 7;
+const npostiPlatea = 10;
+const nfilePalchi = 4;
+const npostiPalchi = 6;
+const teatro = {
   platea: [],
-  palchi: []
+  palchi: [],
+};
+for (var i = 0; i < nfilePlatea; i++) {
+  teatro.platea.push(new Array(npostiPlatea).fill('x'));
 }
-palchi = new ordine(4, 6, 'palchi');
-palchi.posti[2][2].value = 'Luigi';
-platea = new ordine(8, 8, 'platea');
-platea.posti[2][1].value = 'Alessio';
-platea.posti[3][4].value = 'Gianna';
+for (var i = 0; i < nfilePalchi; i++) {
+  teatro.palchi.push(new Array(npostiPalchi).fill('x'));
+}
+var plateaPrenotazione;
+var palchiPrenotazione;
+
+platea[2][1] = 'Alessio';
+platea[3][4] = 'Gianna';
+palchi[4][1] = 'Luigi';
+plateaPrenotazione = new ordine(4, 6, 'platea');
+palchiPrenotazione = new ordine(8, 8, 'palchi');
