b513d89e20364db8bfe5db2eb0529ec588299801
AugustoCiuffoletti <augusto@di.unipi.it>
2022-02-22 11:41:41 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ const nomeEl = document.getElementById('nome');
 const nfile = 5;
 const nposti = 10;
 
-const platea = Array(nfile).fill(Array(nposti).fill("x"));
+const platea = Array(nfile);
+for ( var i = 0; i < nfile; i++ ) {
+  platea[i]= new Array(nposti).fill("x");
+}
 platea[2][1] = 'Alessio';
 platea[3][4] = 'Gianna';
+
+console.log(JSON.stringify(platea));
 
 var prenotazione = new Array(nfile).fill(Array(nposti));
 
 prenotazione=platea.map( (fila,i) => {
-  prenotazione[i]=fila.map( (nomi,j) => {
+  p=fila.map( (nome,j) => {
     btn = document.createElement('button');
     plateaEl.appendChild(btn);
-    btn.value=nomi[j];
+    btn.value=nome;
     btn.innerHTML = 'P' +  (j + 1) + (i + 1);
     btn.addEventListener('click', mostraNome);
+    return btn;
   })
-  
+  plateaEl.appendChild(document.createElement('br'));
+  return p;
 })
 
 function mostraNome() {
